fbf1f650605fdbff5a8659ea1288f0ea9a9653cb
jacobpclouse <clouse.jp@gmail.com>
2022-09-03 17:57:52 -0400

    added main index file and created templates folder, will add more by following guidelines in flask documentation

---
+++ backend/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+# Use flask templates for logic, linking and overall tutorials
+# https://www.digitalocean.com/community/tutorials/how-to-use-templates-in-a-flask-application#step-4-using-conditionals-and-loops
+# THEN build out backend, add frontend/then bootstrap and angular
