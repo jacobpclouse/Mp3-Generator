303c3ba115f0733a2abe43bebc90a06e6657e545
jacobpclouse <clouse.jp@gmail.com>
2022-09-19 12:39:23 -0400

    added all the styles to help fill out the front end

--- backend/app.py
+++ backend/app.py
@@ -3,9 +3,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+# @app.route('/index')
+# def hello():
+#     return render_template('index.html')
+
+
 @app.route('/')
-def hello():
-    return render_template('index.html')
+def mainRoute():
+    return render_template('base.html')
 
 # Use flask templates for logic, linking and overall tutorials
 # https://www.digitalocean.com/community/tutorials/how-to-use-templates-in-a-flask-application#step-4-using-conditionals-and-loops
