71643ad6473e50501a274d85ef2e3da5219faff4
jacobpclouse <clouse.jp@gmail.com>
2022-09-20 12:17:09 -0400

    sucessfully extended the upload page off the base page and passed the title into it

--- backend/app.py
+++ backend/app.py
@@ -2,15 +2,24 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+# Routes
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+@app.route('/')
+def mainRouteFunc():
+    return render_template('base.html')
+
+@app.route('/upload')
+def uploadRouteFunc():
+    title = "Upload Image to Convert"
+    return render_template('upload.html', html_title = title)
+
 
 # @app.route('/index')
 # def hello():
 #     return render_template('index.html')
 
-
-@app.route('/')
-def mainRoute():
-    return render_template('base.html')
 
 # Use flask templates for logic, linking and overall tutorials
 # https://www.digitalocean.com/community/tutorials/how-to-use-templates-in-a-flask-application#step-4-using-conditionals-and-loops
