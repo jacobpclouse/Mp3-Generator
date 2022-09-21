72e3186d4420c1034187a1bf7ae4a6d9cf9a7219
jacobpclouse <clouse.jp@gmail.com>
2022-09-21 08:39:15 -0400

    removed redundant return statement and can upload files without the page giving 304 error

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ A web app that uses Python, Flask, Angular and more to generate mp3s from scans 
 ## Resources Used:
 - Flask and Templates: https://www.digitalocean.com/community/tutorials/how-to-use-templates-in-a-flask-application#step-4-using-conditionals-and-loops
 - How To Create Your First Web Application Using Flask and Python 3: https://www.digitalocean.com/community/tutorials/how-to-create-your-first-web-application-using-flask-and-python-3
-- Uploading files to flask: https://pythonbasics.org/flask-upload-file/
+- Uploading files to flask: https://pythonbasics.org/flask-upload-file/ AND https://flask.palletsprojects.com/en/2.2.x/patterns/fileuploads/#a-gentle-introduction
 - Debug Mode: https://stackoverflow.com/questions/52162882/set-flask-environment-to-development-mode-as-default
 - Upload Image to Flask: https://stackoverflow.com/questions/44926465/upload-image-in-flask
 - Setting up Bootstrap: https://getbootstrap.com/docs/4.3/getting-started/introduction/
--- backend/app.py
+++ backend/app.py
@@ -35,6 +35,7 @@ def mainRouteFunc():
 # Uploading 
 @app.route('/upload',methods=['GET', 'POST'])
 def upload_file():
+    uploaded_file = ''
     title = "Upload Image to Convert"
     
     if request.method == 'POST':
@@ -51,11 +52,38 @@ def upload_file():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            uploaded_file = filename
             #return redirect(url_for('download_file', name=filename))
-            return redirect(url_for('/', name=filename))
+            
 
+
+    return render_template('upload.html', html_title = title, html_file = uploaded_file)
+
+
+"""
+@app.route('/upload',methods=['GET', 'POST'])
+def upload_file():
+    title = "Upload Image to Convert"
+    
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            print(filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('download_file', name=filename))
 
     return render_template('upload.html', html_title = title)
+"""
 
 
 # @app.route('/index')
