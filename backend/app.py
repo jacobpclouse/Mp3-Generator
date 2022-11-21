579d4a5fd1d12cd3f2198e6ba1f1ff3765da46e7
jacobpclouse <clouse.jp@gmail.com>
2022-11-21 12:33:59 -0500

    made it so that file will be decrypted and downloaded automatically

--- backend/app.py
+++ backend/app.py
@@ -24,6 +24,7 @@ from pathlib import Path # used to delete old files in folder
 # Folder to save upload photos to and file types 
 UPLOAD_FOLDER = './UPLOADS'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+DECRYPT_ALLOWED_EXTENSIONS = set(['mp3'])
 
 # Folder to save files that I will be sending to the user
 OUTBOUND_FOLDER = './OUTBOUND'
@@ -82,6 +83,16 @@ def emptyFolder(directoryPath):
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+# used to find ending type for file AND checking to make sure that it is an allowed type
+def decrypt_allowed_file(filename):
+    extension = filename.rsplit('.', 1)[1].lower()
+    period = '.' in filename
+    output= '.' in filename and filename.rsplit('.', 1)[1].lower() in DECRYPT_ALLOWED_EXTENSIONS
+    print(f"Output is: {output}")
+    print(f"Decrypt Ext is: {extension}")
+    print(f"Has period? : {period}")
+    return output
 
 
 # used to construct text message and send it out via email gateway (from guide: https://www.alfredosequeida.com/blog/how-to-send-text-messages-for-free-using-python-use-python-to-send-text-messages-via-email/)
@@ -416,10 +427,67 @@ def signup():
 
 
 
-@app.route('/decrypt')
+@app.route('/decrypt',methods = ['GET','POST'])
 def decrypt():
     title = "decrypt is a work in progress!!"
-    return render_template('base.html', html_title = title)
+
+
+    uploaded_file = ''
+    title = "Upload Image to Decrypt"
+    
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and decrypt_allowed_file(file.filename):
+            # GRABBING FORM INFO -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+            # getting input with name = fname in HTML form
+        # getting input with key = html_user_key in HTML form
+            userKey = request.form.get("html_user_key")
+            print(f"User's Key is: {userKey}")
+
+            secureTheFile = secure_filename(file.filename)
+            extensionType = getExtension(secureTheFile)
+
+            # Filename below - Important for functions 
+            filename = "Temp_Converted_MP3_Upload." + extensionType
+
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            # uploaded_file = secureTheFile
+            # print(uploaded_file)
+
+            ## TAKING KEY VALUE AND PASSING THROUGH Fernet
+            f = Fernet(userKey)
+
+
+            ## OPENING UP ENCRYPTED MESSAGE
+            with open(f'./UPLOADS/{filename}', 'rb') as encrypted_file:
+                encrypted = encrypted_file.read()
+
+            ## DECRYPTING
+            decrypted = f.decrypt(encrypted)
+
+            ## SAVING DECRYPTED MESSAGE
+            with open('./UPLOADS/decryptedMessage.mp3', 'wb') as decrypted_file:
+                decrypted_file.write(decrypted)
+
+            """ This Will let the user download the file, then deletes all files in outbound and uploads """
+            try:
+              
+                return send_from_directory(UPLOAD_FOLDER,"decryptedMessage.mp3",as_attachment=True)
+                
+            except FileNotFoundError:
+                os.abort(404)
+
+
+    return render_template('decrypt.html', html_title = title)
 
 # Use flask templates for logic, linking and overall tutorials
 # https://www.digitalocean.com/community/tutorials/how-to-use-templates-in-a-flask-application#step-4-using-conditionals-and-loops
